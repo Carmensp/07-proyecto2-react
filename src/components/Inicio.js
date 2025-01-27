df7e92a774c3ee7d49c962bbb7c30e7bb88ea4a5
carmensp <carmen.8.2.0.3.sp@gmail.com>
2025-01-27 13:10:00 +0100

    segundo commit

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div className='home'>
+      <h1>
+        <strong>Full Stack Developer</strong> en Madrid. Soy Carmen Sánchez y me gustaría 
+        aportar mis conocimientos sobre el <strong>desarrollo</strong>.
+      </h1>
+      <h2>
+        Te ayudo a crear tu sitio o aplicación multiplataforma, tener más visibilidad 
+        y relevancia en internet. <Link to="/contacto">Contacta conmigo</Link>
+      </h2>
+      <section className='last-works'>
+        <h2 className='heading'>Algunos de mis proyectos</h2>
+        <p>A continuación se muestran algunos de mis trabajos de desarrollo software.</p>
+        <div className='works'>
+
+        </div>
+      </section>
+    </div>
   )
 }
--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const Servicios = () => {
   return (
-    <div>Servicios</div>
+    <div className='page'>
+      <h1 className='heading'>Servicios</h1>
+      <section className='services'>
+        <article className='service'>
+          <h2>Diseño Web</h2>
+          <p>Doy forma y originalidad a la interfaz de tu web.</p>
+        </article>
+        <article className='service'>
+          <h2>Desarrollo Software</h2>
+          <p>Utilizando lenguajes como Java, Python JavaScript.</p>
+        </article>
+        <article className='service'>
+          <h2>Gestión de pryecto</h2>
+          <p>Utilizando herramientas como Git para la gestión del proyecto.</p>
+        </article>
+      </section>
+    </div>
   )
 }
